cfbd50081a15a5f17dd72c83efd7538e76ebe451
Маргарита <nabinonabi090908@gmail.com>
2026-06-26 19:31:58 +0300

    Fix staticfiles storage and check static dir path

--- turizm/settings.py
+++ turizm/settings.py
@@ -80,10 +80,15 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
+
+if (BASE_DIR / 'static').exists():
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
